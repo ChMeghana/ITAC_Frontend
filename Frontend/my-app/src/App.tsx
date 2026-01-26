a3db184d4bb1db05ad1dd0460dd017ee83788b90
Shamsheer Ali <80998199+ShamsheerAli@users.noreply.github.com>
2026-01-26 06:26:09 +0000

    Staff pages udpate

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -9,6 +9,14 @@ import Inbox from './pages/Inbox'
 import UpdateDetails from './pages/ClientUpdateDetails'
 import UploadDocuments from './pages/UploadDocuments'
 import DashboardLayout from './layouts/DashboardLayout' // Import the new layout
+import StaffDashboard from './pages/StaffDashboard';
+import StaffLayout from './layouts/StaffLayout';
+import StaffKanban from './pages/StaffKanban';
+import StaffMyInformation from './pages/StaffMyInformation';
+import StaffAddClient from './pages/StaffAddClient';
+import StaffClientReview from './pages/StaffClientReview';
+import StaffDocumentReview from './pages/StaffDocumentReview';
+import StaffInbox from './pages/StaffInbox';
 
 function App() {
   return (
@@ -26,12 +34,23 @@ function App() {
         {/* We wrap these inside the Layout route */}
         <Route element={<DashboardLayout />}>
         <Route path="/dashboard" element={<Dashboard />} />
-        
 
         <Route path="/inbox" element={<Inbox />} />
           <Route path="/update-details" element={<UpdateDetails />} />
           <Route path="/upload-documents" element={<UploadDocuments />} />
           {/* You can add more sidebar pages here later */}
+        </Route>
+        <Route element={<StaffLayout />}>
+          <Route path="/staff-dashboard" element={<StaffDashboard />} />
+          <Route path="/staff-kanban" element={<StaffKanban />} />
+          <Route path="/staff-info" element={<StaffMyInformation />} />
+          <Route path="/add-new-client" element={<StaffAddClient />} />
+          <Route path="/staff-client-review/:clientId" element={<StaffClientReview />} />
+          <Route path="/staff-document-review/:clientId" element={<StaffDocumentReview />} />
+          <Route path="/staff-inbox/:clientId?" element={<StaffInbox />} />
+          {/* Add other staff pages here later, e.g.:
+          <Route path="/staff-kanban" element={<KanbanBoard />} /> 
+          */}
         </Route>
       </Routes>
     </>
